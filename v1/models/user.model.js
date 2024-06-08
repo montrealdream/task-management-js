3da4b8eb484d7c160bea02f2966f679131fb4f95
giangtruongjs <hogiangtruongbtre@gmail.com>
2024-06-08 20:41:24 +0700

    model cho user

---
+++ v1/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// defining shema
+const UserSchema = new mongoose.Schema(
+    {
+        fullName: String,
+        email: String,
+        avatar: String,
+        tel: String,
+        tokenUser: String,
+        password: String,
+        deleted: {
+            type: Boolean,
+            default: false
+        },
+        deletedAt: Date
+    },
+    {
+        timestamps: true
+    }
+);
+
+// create model
+const User = mongoose.model('User', TaskSchema, 'user');
+
+// export
+module.exports = User;
